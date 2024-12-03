30e70db7c2c71a330d60868dd17f989f1e8153d7
victoralfonso1977 <victoralfonso1977@yahoo.com>
2024-12-03 11:26:47 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import DailyBugle from './components/DailyBugle'
+
+function App() {
+  // The App component serves as our main container
+  // It simply returns our DailyBugle component
+  return <DailyBugle />
+}
+
+export default App
